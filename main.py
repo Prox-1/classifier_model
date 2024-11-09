44a2d43b78dc8d7563e85d47ef023cf549bb56eb
Prox-1 <kurganovdanka@gmail.com>
2024-11-09 13:19:12 +0300

    update user interface

--- main.py
+++ main.py
@@ -2,20 +2,37 @@ import eel
 import torch
 import base64
 import io
-from PIL import Image
+import torch.nn as nn
+import torchvision.models as models
 import torchvision.transforms as transforms
+from PIL import Image
 from Net_class import Net, classes
 
 eel.init('web')
-net = Net()
-net.load_state_dict(torch.load("cifar_net_1_new.pth", weights_only=True))
-net.eval()
+resnet = models.resnet18(weights='DEFAULT')
+resnet.fc = nn.Linear(resnet.fc.in_features, 10) 
+resnet.load_state_dict(torch.load("models/cifar_net_resnet.pth", weights_only=True))
+resnet.eval()
+resnet_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
-my_data_transform = transforms.Compose([
+net = Net()
+net.load_state_dict(torch.load("models/cifar_net_1_new_amsgrad.pth", weights_only=True))
+net.eval()
+net_transform = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
+
+def model_selection(model_name):
+    if model_name == 'resnet':
+        return resnet, resnet_transform
+    elif model_name == 'net':
+        return net, net_transform
 
 def load_image_from_file(file_content):
     header, encoded = file_content.split(',', 1)
@@ -23,13 +40,13 @@ def load_image_from_file(file_content):
     img = Image.open(io.BytesIO(data)).convert('RGB')
     return img
 
-def transform_image(img):
-    return my_data_transform(img)
+def transform_image(img, transform):
+    return transform(img)
 
-def predict_class(img_t):
+def predict_class(img_t, model):
     with torch.no_grad():
-        class_probabilities = net(img_t.unsqueeze(0))
-        pred = torch.argmax(class_probabilities)
+        class_probabilities = model(img_t.unsqueeze(0))
+        pred = torch.argmax(class_probabilities, 1)
     return pred, class_probabilities
 
 def tensor_to_base64(img_t):
@@ -45,24 +62,25 @@ def tensor_to_base64(img_t):
 def normalize_and_reverse_probabilities(probabilities):
     if not probabilities:
         return []
-    positive_probabilities = [-p for p in probabilities]
-    min_prob = min(positive_probabilities)
-    max_prob = max(positive_probabilities)
-    adjusted_probabilities = [(max_prob - (p - min_prob)) for p in positive_probabilities]
-    total_adjusted = sum(adjusted_probabilities)
-    normalized = [(p / total_adjusted) * 100 for p in adjusted_probabilities]
-
-    return normalized
+    min_prob = min(probabilities)
+    if min_prob < 0:
+        probabilities = [p - min_prob for p in probabilities]
+    total = sum(probabilities)
+    if total == 0:
+        return [0] * len(probabilities)
+    percentages = [(p / total) * 100 for p in probabilities]
+    return percentages
 
 
 @eel.expose
-def make_pred(file_content):
-    img = load_image_from_file(file_content) 
-    img_t = transform_image(img)  
-    pred, class_probabilities = predict_class(img_t)  
+def make_pred(file_content, model_name):
+    img = load_image_from_file(file_content)
+    model, transform = model_selection(model_name) 
+    img_t = transform_image(img, transform)  
+    pred, class_probabilities = predict_class(img_t, model)  
     
     class_probabilities = normalize_and_reverse_probabilities(class_probabilities.squeeze().tolist())  
 
     return [int(pred), class_probabilities]
 
-eel.start('main.html', size=(700, 700))
+eel.start('main.html', size=(800, 700))
--- web/functional.js
+++ web/functional.js
@@ -1,52 +1,120 @@
-async function makePrediction(fileContent) {
-    const classes = ['Самолет','Автомобиль','Птица','Кот','Олень','Собака','Лягушка','Лошадь','Лодка','Грузовик'];
-    const result = await eel.make_pred(fileContent)();
-    const res = classes[result[0]];  // Предсказанный класс
-    const invertedProbabilities = result[1];  // Инвертированный массив вероятностей
-    document.getElementById('pred').textContent = "Предсказан класс: " + res;
-    const probabilitiesDisplay = document.getElementById('class_probabilities');
-    probabilitiesDisplay.innerHTML = "Вероятности:<br>"; // Начинаем с заголовка
-    // Цикл для отображения вероятностей
-    invertedProbabilities.forEach((prob, index) => {
-        const percentage = prob; // То, что мы показываем, уже есть в prob
-        // Создаем элемент для линии и задаем стили
-        const line = document.createElement('div');
-        line.style.width = `${percentage}%`; // Длина линии в зависимости от процента
-        line.style.height = '15px'; // Высота линии
-        line.style.backgroundColor = '#4CAF50'; // Цвет линии
-        line.style.margin = '2px 0'; // Отступ между линиями
-        line.style.borderRadius = '3px'; // Сглаженные углы
-        // Добавляем текст с классом и процентом
-        const label = document.createElement('span');
-        label.textContent = `${classes[index]}: ${percentage.toFixed(2)}%`;
-        label.style.marginRight = '10px'; // Отступ справа от текста
-        label.style.verticalAlign = 'middle'; // Центрирование текста по вертикали
-        // Создаем контейнер для текста и линии
-        const container = document.createElement('div');
-        container.style.display = 'flex'; // Используем flexbox для горизонтального выравнивания
-        container.appendChild(label);
-        container.appendChild(line);
-        // Добавляем контейнер в вывод
-        probabilitiesDisplay.appendChild(container);
-    })
+async function makePrediction(fileContent, className) {
+  const classes = [
+    "Самолет",
+    "Автомобиль",
+    "Птица",
+    "Кот",
+    "Олень",
+    "Собака",
+    "Лягушка",
+    "Лошадь",
+    "Лодка",
+    "Грузовик",
+  ];
+  const result = await eel.make_pred(fileContent, className)();
+  const res = classes[result[0]]; // Предсказанный класс
+  const invertedProbabilities = result[1]; // Инвертированный массив вероятностей
+  document.getElementById("pred").textContent = "Это: " + res;
+  const probabilitiesDisplay = document.getElementById("class_probabilities");
+  probabilitiesDisplay.innerHTML = "Вероятности:<br>"; // Начинаем с заголовка
+  // Цикл для отображения вероятностей
+  invertedProbabilities.forEach((prob, index) => {
+    const percentage = prob; // То, что мы показываем, уже есть в prob
+    // Создаем элемент для линии и задаем стили
+    const line = document.createElement("div");
+    line.style.width = `${percentage}%`; // Длина линии в зависимости от процента
+    line.style.height = "15px"; // Высота линии
+    line.style.backgroundColor = "#4CAF50"; // Цвет линии
+    line.style.margin = "2px 0"; // Отступ между линиями
+    line.style.borderRadius = "3px"; // Сглаженные углы
+    // Добавляем текст с классом и процентом
+    const label = document.createElement("span");
+    label.textContent = `${classes[index]}: ${percentage.toFixed(2)}%`;
+    label.style.marginRight = "10px"; // Отступ справа от текста
+    label.style.verticalAlign = "middle"; // Центрирование текста по вертикали
+    // Создаем контейнер для текста и линии
+    const container = document.createElement("div");
+    container.style.display = "flex"; // Используем flexbox для горизонтального выравнивания
+    container.appendChild(label);
+    container.appendChild(line);
+    // Добавляем контейнер в вывод
+    probabilitiesDisplay.appendChild(container);
+  });
 }
 
-document.getElementById('uploadButton').addEventListener('click', function() {
-    const fileInput = document.getElementById('fileUpload');
+function setActiveButton(value) {
+  // Убираем класс active с обеих кнопок
+  document.getElementById("button1").classList.remove("active");
+  document.getElementById("button2").classList.remove("active");
+
+  // Устанавливаем новый активный класс нажатой кнопке
+  if (value === "net") {
+    document.getElementById("button1").classList.add("active");
+  } else {
+    document.getElementById("button2").classList.add("active");
+  }
+
+  // Изменение значения переменной
+  className = value;
+  console.log("Текущая активная кнопка:", activeValue);
+}
+let className = "";
+
+document.getElementById("button1").addEventListener("click", function () {
+  setActiveButton("net");
+});
+document.getElementById("button2").addEventListener("click", function () {
+  setActiveButton("resnet");
+});
+
+document.getElementById("uploadButton").addEventListener("click", function () {
+  if (className != "") {
+    const fileInput = document.getElementById("fileUpload");
     const file = fileInput.files[0];
     if (file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const fileContent = event.target.result; // Читаем содержимое файла
+
+        const uploadedImage = document.getElementById("uploadedImage");
+        const pred_div = document.getElementById("pred_div");
+        makePrediction(fileContent, className);
+        uploadedImage.src = fileContent;
+        pred_div.style.display = "flex";
+      };
+
+      reader.readAsDataURL(file); // Считываем файл как data URL
+    } else {
+      alert("Пожалуйста, выберите файл.");
+    }
+  } else {
+    alert("Пожалуйста, выберите модель.");
+  }
+});
+
+document.getElementById("fileUpload").addEventListener("change", function () {
+  if (this.files.length > 0) {
+    if (className != "") {
+      const fileInput = document.getElementById("fileUpload");
+      const file = fileInput.files[0];
+      if (file) {
         const reader = new FileReader();
-        reader.onload = function(event) {
-            const fileContent = event.target.result; // Читаем содержимое файла
-            
-            const uploadedImage = document.getElementById('uploadedImage');
-            uploadedImage.src = fileContent; 
-            uploadedImage.style.display = 'inline'//!!!!!!!!!!!!!!!!!!!!!!!; 
-            makePrediction(fileContent);
+        reader.onload = function (event) {
+          const fileContent = event.target.result; // Читаем содержимое файла
+
+          const uploadedImage = document.getElementById("uploadedImage");
+          const pred_div = document.getElementById("pred_div");
+          makePrediction(fileContent, className);
+          uploadedImage.src = fileContent;
+          pred_div.style.display = "flex";
         };
 
         reader.readAsDataURL(file); // Считываем файл как data URL
-    } else {
+      } else {
         alert("Пожалуйста, выберите файл.");
+      }
+    } else {
+      alert("Пожалуйста, выберите модель.");
     }
+  }
 });
